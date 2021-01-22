300821667f3bcd9af6b161f5c92a6e1a8546520b
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-01-22 13:49:06 -0300

    :fire: feat: FindById() implementation

---
+++ src/main/java/com/ghostdovahkiin/LibrApi/user/services/ListOneUserImpl.java
@@ -0,0 +1,19 @@
+package com.ghostdovahkiin.LibrApi.user.services;
+
+import com.ghostdovahkiin.LibrApi.exceptions.UserNotFoundException;
+import com.ghostdovahkiin.LibrApi.user.User;
+import com.ghostdovahkiin.LibrApi.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ListOneUserImpl implements ListOneUserService{
+    private final UserRepository userRepository;
+
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+    }
+}
