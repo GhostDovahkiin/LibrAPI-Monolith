e025d30816dc33d3cb9972d6244e12302a746ed5
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-01-22 15:50:09 -0300

    :fire: feat: User Entity V1

--- src/main/java/com/ghostdovahkiin/LibrApi/user/v1/UserController.java
+++ src/main/java/com/ghostdovahkiin/LibrApi/user/v1/UserController.java
@@ -2,15 +2,12 @@ package com.ghostdovahkiin.LibrApi.user.v1;
 
 import com.ghostdovahkiin.LibrApi.user.User;
 import com.ghostdovahkiin.LibrApi.user.UserDTO;
-
-import com.ghostdovahkiin.LibrApi.user.services.ListAllUserService;
-import com.ghostdovahkiin.LibrApi.user.services.ListOneUserService;
+import com.ghostdovahkiin.LibrApi.user.services.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -20,14 +17,39 @@ public class UserController {
 
     private final ListAllUserService listAllUserService;
     private final ListOneUserService listOneUserService;
+    private final SaveUserService saveUserService;
+    private final DeleteUserService deleteUserService;
+    private final UpdateUserService updateUserService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<UserDTO> findAll() {
         return UserDTO.fromAll(listAllUserService.findAll());
     }
 
     @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public UserDTO findByID(@PathVariable Long id) {
         return UserDTO.from(listOneUserService.findById(id));
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@Valid @RequestBody UserDTO userDTO){
+        saveUserService.save(User.to(userDTO));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        deleteUserService.delete(id);
+    }
+
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void update(@Valid @RequestBody UserDTO userDTO, @PathVariable Long id){
+        updateUserService.update(User.to(userDTO), id);
+    }
+
+
 }
