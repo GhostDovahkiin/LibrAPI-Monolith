94934364b0f989efac54352c69c1edd3f8b73e5d
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-01-22 13:49:26 -0300

    :fire: feat: UserController with GET

--- src/main/java/com/ghostdovahkiin/LibrApi/user/v1/UserController.java
+++ src/main/java/com/ghostdovahkiin/LibrApi/user/v1/UserController.java
@@ -1,26 +1,33 @@
-package com.ghostdovahkiin.LibrApi.resource;
+package com.ghostdovahkiin.LibrApi.user.v1;
 
-import com.ghostdovahkiin.LibrApi.user.UserRepository;
 import com.ghostdovahkiin.LibrApi.user.User;
+import com.ghostdovahkiin.LibrApi.user.UserDTO;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import com.ghostdovahkiin.LibrApi.user.services.ListAllUserService;
+import com.ghostdovahkiin.LibrApi.user.services.ListOneUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
-public class UserResource {
+@RequiredArgsConstructor
+@RequestMapping("/v1/api/users")
+public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final ListAllUserService listAllUserService;
+    private final ListOneUserService listOneUserService;
 
     @GetMapping
-    public ResponseEntity<Object> list() {
-        List<User> usersFound = userRepository.findAll();
-        return !usersFound.isEmpty() ? ResponseEntity.ok(usersFound) : ResponseEntity.noContent().build();
+    public List<UserDTO> findAll() {
+        return UserDTO.fromAll(listAllUserService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public UserDTO findByID(@PathVariable Long id) {
+        return UserDTO.from(listOneUserService.findById(id));
     }
 }
