6b288ef8d8f3ea86b9b14d0811f053b4a3c68e68
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-01-27 16:12:31 -0300

    :fire: feat: DeleteUser Test

--- src/test/java/com/ghostdovahkiin/LibrApi/user/services/DeleteUserServiceTest.java
+++ src/test/java/com/ghostdovahkiin/LibrApi/user/services/DeleteUserServiceTest.java
@@ -1,5 +1,6 @@
 package com.ghostdovahkiin.LibrApi.user.services;
 
+import com.ghostdovahkiin.LibrApi.exceptions.UserNotFoundException;
 import com.ghostdovahkiin.LibrApi.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -7,6 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static com.ghostdovahkiin.LibrApi.user.services.builders.UserBuilder.createUser;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Tests execution for Delete User Service")
@@ -23,6 +31,19 @@ public class DeleteUserServiceTest {
     @Test
     @DisplayName("Should delete a User")
     void shouldDeleteUser() {
-
+        when(userRepository.existsById(anyLong())).thenReturn(true);
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(createUser().build()));
+        deleteUserService.delete(1L);
+        verify(userRepository).existsById(anyLong());
     }
+
+    @Test
+    @DisplayName("Shold return a UserNotFoundException if not encountered a user with specified ID")
+    void shouldThrowUserNotFoundException() {
+        when(userRepository.existsById(anyLong())).thenReturn(false);
+        assertThrows(UserNotFoundException.class, () -> deleteUserService.delete(1L));
+        verify(userRepository, times(0)).deleteById(anyLong());
+    }
+
+
 }
