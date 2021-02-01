c793935784dd60e65519ce901c5524bd00a2ccc9
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-02-01 19:22:39 -0300

    :recycle: refactor: Update Service PASS

--- src/test/java/com/ghostdovahkiin/LibrApi/user/services/UpdateUserServiceTest.java
+++ src/test/java/com/ghostdovahkiin/LibrApi/user/services/UpdateUserServiceTest.java
@@ -1,6 +1,5 @@
 package com.ghostdovahkiin.LibrApi.user.services;
 
-import com.ghostdovahkiin.LibrApi.user.Sex;
 import com.ghostdovahkiin.LibrApi.user.User;
 import com.ghostdovahkiin.LibrApi.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,20 +9,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.Optional;
 
 import static com.ghostdovahkiin.LibrApi.user.services.builders.UserBuilder.createUser;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.times;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Tests execution for Update User Service")
-public class UpdateUserServiceTest {
+class UpdateUserServiceTest {
     @Mock
     private UserRepository userRepository;
     private UpdateUserServiceImpl updateUserService;
@@ -36,21 +34,29 @@ public class UpdateUserServiceTest {
     @Test
     @DisplayName("Should update a user")
     void shouldUpdateUser() {
-        when(userRepository.findById(145485989485039832L)).thenReturn(Optional.of(createUser().id(145485989485039832L).build()));
 
-        updateUserService.update(createUser().phone("UPDATED PHONE").name("UPDATED NAME").age(22).email("UPDATED.EMAIL.COM").sex(Sex.MASCULINO).build(), 145485989485039832L);
+        User userToUpdate = createUser()
+                .name("Name update")
+                .age(30)
+                .build();
 
-        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
-        verify(userRepository, times(1)).save(userCaptor.capture());
+        User user = createUser().build();
+        Optional<User> clientOptional  = Optional.of(user);
+        when(userRepository.findById(anyLong())).thenReturn(clientOptional);
 
-        User resultUser = userCaptor.getValue();
+        updateUserService.update(userToUpdate, 1L);
 
-        assertAll("User",
-                () -> assertThat(resultUser.getName(), is("UPDATED NAME")),
-                () -> assertThat(resultUser.getPhone(), is("UPDATED PHONE")),
-                () -> assertThat(resultUser.getAge(), is(22)),
-                () -> assertThat(resultUser.getEmail(), is("UPDATED.EMAIL.COM")),
-                () -> assertThat(resultUser.getSex(), is(Sex.MASCULINO))
+        ArgumentCaptor<User> clientArgumentCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(clientArgumentCaptor.capture());
+
+        User result = clientArgumentCaptor.getValue();
+
+        assertAll("Client",
+                () -> assertThat(result.getName(), is(userToUpdate.getName())),
+                () -> assertThat(result.getAge(), is(userToUpdate.getAge())),
+                () -> assertThat(result.getEmail(), is(userToUpdate.getEmail())),
+                () -> assertThat(result.getPhone(), is(userToUpdate.getPhone())),
+                () -> assertThat(result.getSex(), is(userToUpdate.getSex()))
         );
     }
 }
