75dc21c6e9106a19b28683da690004adf6410a1f
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-02-03 08:48:23 -0300

    :fire: feat: bookCategoryRepository

---
+++ src/main/java/com/ghostdovahkiin/LibrApi/bookCategory/bookCategoryRepository.java
@@ -0,0 +1,6 @@
+package com.ghostdovahkiin.LibrApi.bookCategory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface bookCategoryRepository extends JpaRepository<bookCategory, Long> {
+}
