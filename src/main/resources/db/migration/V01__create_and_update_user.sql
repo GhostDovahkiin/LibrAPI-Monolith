a42dd30d6d2206e8f54f6b1526464fb6692e43e9
GhostDovahkiin <pedro.sousa@dcx.ufpb.br>
2021-02-10 13:49:06 -0300

    :recycle: refactor: Make user phone and email UNIQUE

--- src/main/resources/db/migration/V01__create_and_update_user.sql
+++ src/main/resources/db/migration/V01__create_and_update_user.sql
@@ -2,10 +2,10 @@ CREATE TABLE users(
     id BIGSERIAL NOT NULL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     age INTEGER NOT NULL,
-    phone VARCHAR(15) NOT NULL,
-    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(15) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
     sex VARCHAR(50) NOT NULL
 );
 
-INSERT INTO users(name, age, phone, email, sex) VALUES ('Pedro', 22, '+5583986862912', 'pedro.sousa@phoebus.com.br', 'MASCULINO');
+INSERT INTO users(name, age, phone, email, sex) VALUES ('Pedro', 22, '+558398662912', 'pedro.sousa@phoebus.com.br', 'MASCULINO');
 INSERT INTO users(name, age, phone, email, sex) VALUES ('Lucas', 21, '+5583986862912', 'lucas.antonio@phoebus.com.br', 'MASCULINO');
