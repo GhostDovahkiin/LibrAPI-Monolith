fc9eecbae34470b7ed5aca79619ac2c8f6f499f3
GhostDovahkiin <pedro.sousa@dcx.ufpb.br>
2021-02-10 13:51:28 -0300

    :recycle: refactor: Make book ISBN unique, publicationYear is DATE and increment Synopsis size

--- src/main/resources/db/migration/V03__create_and_update_book.sql
+++ src/main/resources/db/migration/V03__create_and_update_book.sql
@@ -1,10 +1,10 @@
 CREATE TABLE book(
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    isbn VARCHAR(17) NOT NULL,
+    isbn VARCHAR(17) NOT NULL UNIQUE,
     title VARCHAR(50) NOT NULL,
-    synopsis VARCHAR(120) NOT NULL,
+    synopsis VARCHAR(500) NOT NULL,
     author VARCHAR(50) NOT NULL,
-    publication_year INTEGER NOT NULL,
+    publication_year DATE NOT NULL,
     sell_price DECIMAL NOT NULL,
     available_quantity INTEGER NOT NULL,
     category_id INTEGER NOT NULL,
