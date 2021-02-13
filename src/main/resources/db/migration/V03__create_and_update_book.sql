6bd64615a8c42342a212bc76a6b3ce58b26e3190
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-02-13 00:54:12 -0300

    :fire: feat: Update Book + Livro relationship in FlywayDB

--- src/main/resources/db/migration/V03__create_and_update_book.sql
+++ src/main/resources/db/migration/V03__create_and_update_book.sql
@@ -1,12 +1,10 @@
 CREATE TABLE book(
-    id BIGSERIAL NOT NULL PRIMARY KEY,
+    book_id BIGSERIAL NOT NULL PRIMARY KEY,
     isbn VARCHAR(17) UNIQUE NOT NULL,
     title VARCHAR(50) NOT NULL,
     synopsis VARCHAR(500) NOT NULL,
     author VARCHAR(50) NOT NULL,
     publication_year DATE NOT NULL,
     sell_price DECIMAL(10, 2) NOT NULL,
-    available_quantity INTEGER NOT NULL,
-    category_id INTEGER,
-    FOREIGN KEY (category_id) REFERENCES category(id)
+    available_quantity INTEGER NOT NULL
 );
