1448d86a8ebb47f4e793c9803809ecb0e7af60c2
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-02-17 10:02:13 -0300

    :recycle: refactor: Organize Book imports

--- src/main/java/com/ghostdovahkiin/librapi/book/Book.java
+++ src/main/java/com/ghostdovahkiin/librapi/book/Book.java
@@ -8,14 +8,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
