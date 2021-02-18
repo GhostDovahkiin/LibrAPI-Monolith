92f838f14b419eecb21403ab33b3d17373a24859
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-02-18 08:59:46 -0300

    :fire: feat: Set purchaseStatus to PENDING when save

--- src/main/java/com/ghostdovahkiin/librapi/purchase/services/SavePurchaseServiceImpl.java
+++ src/main/java/com/ghostdovahkiin/librapi/purchase/services/SavePurchaseServiceImpl.java
@@ -2,16 +2,17 @@ package com.ghostdovahkiin.librapi.purchase.services;
 
 import com.ghostdovahkiin.librapi.purchase.PurchaseRepository;
 import com.ghostdovahkiin.librapi.purchase.Purchase;
+import com.ghostdovahkiin.librapi.purchase.PurchaseStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class SavePurchaseServiceImpl implements SavePurchaseService{
-    private final PurchaseRepository pucharseRepository;
+    private final PurchaseRepository purchaseRepository;
 
     @Override
     public void save(Purchase purchase) {
-        pucharseRepository.save(purchase);
+        purchaseRepository.save(purchase).setPurchaseStatus(PurchaseStatus.PENDING);
     }
 }
